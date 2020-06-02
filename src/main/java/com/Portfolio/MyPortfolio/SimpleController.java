9478bee64049d8bd7f0e231b7dcb076a5158d516
zinahalnajjar <59237103+zinahalnajjar@users.noreply.github.com>
2020-06-01 21:36:44 -0400

    update blah blah

--- src/main/java/com/Portfolio/MyPortfolio/SimpleController.java
+++ src/main/java/com/Portfolio/MyPortfolio/SimpleController.java
@@ -3,15 +3,27 @@ package com.Portfolio.MyPortfolio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class SimpleController {
-	
-	 @GetMapping("/ui/upload")
-	 public String MyPortfolio(Model model) {
+
+	@GetMapping("/ui/upload")
+	public String MyPortfolio(Model model) {
+
+		return "upload";
+
+	}
+
+	@PostMapping("/uploadartwork")
+	public String uploadartwork(@RequestParam("artworkfile") MultipartFile file, Model model) {
 		
-		 return "upload";
-		 
-	 }
-	
+		if (file!= null) 
+			model.addAttribute("Message", "upload successful");
+		return "upload";
+
+	}
+
 }
